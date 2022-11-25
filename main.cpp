640efe3087afcab94b21c23d5078a8cf46b6ef03
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-11-25 12:43:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
 int n=0, primoN=1,secondoN=1,terzoN;
 cin>>n;
+cout<<pirmoN<<endl;
+cout<<secondoN<<endl;
 for (int i=2;i<n;i++ ) {
     terzoN = primoN + secondoN;
     primoN = secondoN;
