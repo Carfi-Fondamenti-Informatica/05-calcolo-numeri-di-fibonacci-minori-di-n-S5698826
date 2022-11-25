826d07fb4b7c229562df6968572fc93c2ef00ebe
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-11-25 12:58:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main() {
 int n=0, primoN=1,secondoN=1,terzoN;
 cin>>n;
-for (int i=2;i<=n;i++ ) {
+cout<<primoN<<endl;
+cout<<secondoN<<endl;     
+for (int i=2;i<=n;i++){
     terzoN = primoN + secondoN;
     primoN = secondoN;
     secondoN = terzoN;
-    if (terzoN <= n) {
-        cout << terzoN << endl;
-    }
-}
+    if (terzoN<=n){
+    cout << terzoN << endl;  
+  }
    return 0;
 }
